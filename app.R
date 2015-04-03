9eab5ced8556fc9e4e65af5be8a3efa887765023
ironholds <ironholds@gmail.com>
2015-04-03 02:25:51 -0400

    finish research

--- app.R
+++ app.R
@@ -7,7 +7,8 @@ content_link <- function(file){
 
 
 server <- function(input, output){
-  output$research_work <- renderDataTable(file.path(getwd(),"assets","other", "papers.tsv"),
+  output$research_work <- renderDataTable(read.delim(file.path(getwd(),"assets","other", "papers.tsv"),
+                                                     as.is = TRUE, header = TRUE),
                                           options = list(autoWidth = FALSE,
                                                          paging = FALSE,
                                                          searching = FALSE))
@@ -37,14 +38,6 @@ ui <- navbarPage(title = "Oliver Keyes", theme = "theme/theme.css",
                                      includeMarkdown(content_link("Research.md")),
                                      h3("Papers and presentations"),
                                      dataTableOutput("research_work")
-                                     )
-                              )
-                            ),
-                   tabPanel("C.V.",
-                            fluidRow(
-                              column(2),
-                              column(6,
-                                     includeMarkdown(content_link("About.md"))
                                      )
                               )
                             ),
---
+++ assets/content/Research.md
@@ -0,0 +1,1 @@
+My research is centred around geodata and reader behavioural patterns: I'm happiest when I have something to localise. 

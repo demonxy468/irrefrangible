223c2fce5faca3053376d4f13302a07098ec3be3
ironholds <ironholds@gmail.com>
2015-04-03 02:40:29 -0400

    strip

--- app.R
+++ app.R
@@ -5,7 +5,6 @@ content_link <- function(file){
   return(file.path(getwd(),"assets","content", file))
 }
 
-
 server <- function(input, output){
   output$research_work <- renderDataTable(read.delim(file.path(getwd(),"assets","other", "papers.tsv"),
                                                      as.is = TRUE, header = TRUE),
@@ -38,14 +37,6 @@ ui <- navbarPage(title = "Oliver Keyes", theme = "theme/theme.css",
                                      includeMarkdown(content_link("Research.md")),
                                      h3("Papers and presentations"),
                                      dataTableOutput("research_work")
-                                     )
-                              )
-                            ),
-                   tabPanel("Blog",
-                            fluidRow(
-                              column(2),
-                              column(6,
-                                     includeMarkdown(content_link("About.md"))
                                      )
                               )
                             )
